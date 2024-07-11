20140d394adbf0273cc03668a7f838eb05fc105d
marioumbelino <marioherbert2016@gmail.com>
2024-07-10 21:05:45 -0300

    Orientada a Objetos v0.1
    Fragmentando o sistema bancário principal em classes, seguindo conceitos de programação orientada a objetos.

---
+++ historico.py
@@ -0,0 +1,6 @@
+class Historico:
+    def __init__(self) -> None:
+        self.transacoes = []
+    
+    def adicionar_transacao(self):
+        pass
---
+++ pessoafisica.py
@@ -0,0 +1,5 @@
+class PessoaFisica:
+    def __init__(self, nome, cpf, data_nascimento):
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
